33527f70cb066324b8df3dfbcf6ae60305016db4
iacd-Jerry <113135078+iacd-Jerry@users.noreply.github.com>
2024-08-12 18:42:20 +0000

    deploy rms site 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "907213157"
+    serviceWorkerVersion: "2499468531"
   }
 });
